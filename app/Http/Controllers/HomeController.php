07f79b7b4ec03435782453264c42661779c19d80
Walgeof <mosk.yu.07@gmail.com>
2026-02-22 17:05:20 +0200

    Add Ukrainian localization on login and register pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $orders = Order::with('items')
+            ->where('user_id', auth()->id())
+            ->latest()
+            ->take(5)
+            ->get();
+
+        return view('home', compact('orders'));
     }
 }
